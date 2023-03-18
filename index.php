a7a7a86b7ded3209fd76fd72fbbc01025e661460
Your Name <you@example.com>
2023-03-18 04:21:48 -0700

    new

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// variabel integer
+$x = 5985;
+var_dump(is_int($x));
+
+echo "<br>";
+
+// Cek sekarang
+$x = 59.85;
+var_dump(is_int($x));
+?>  
+
+</body>
+</html>
